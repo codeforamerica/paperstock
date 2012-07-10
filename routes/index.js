e53199a95f8b3e4f8d0366a27c5b00f048aa5a0a
Ben Sheldon <bensheldon@gmail.com>
2012-07-10 11:20:33 -0700

    Cleaned up the scaffold

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,5 @@
 module.exports = function(req, res) {
   res.render('index', { 
-      title: 'Open311 Status'
-    , endpoints: endpointsData
-    , serviceRequests: serviceRequestsData
+    title: "Paperstock"
   });
 }
